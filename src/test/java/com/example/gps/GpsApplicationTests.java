d5f910d229a24d4e7caff6e959f112bf555c00e0
artem-00 <112750639+artem-00@users.noreply.github.com>
2024-02-19 01:49:23 +0300

    Update GpsApplicationTests.java

--- src/test/java/com/example/gps/GpsApplicationTests.java
+++ src/test/java/com/example/gps/GpsApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GpsApplicationTests {
 
-    @Test
+ @Test
     void contextLoads() {
+        assertNotNull(gpsController);
     }
 
 }
